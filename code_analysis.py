fcf032bec833673c41c73b0b23ee6a9d96e118cc
Dan Gopstein <dan@gopstein.com>
2019-10-31 08:55:36 -0400

    collect codes under snippets

--- code_analysis.py
+++ code_analysis.py
@@ -7,10 +7,13 @@ from matplotlib import pyplot as plt
 plt.ion()
 
 pp = pprint.PrettyPrinter(indent=4)
-snippets = parse_snippets.parse_interviews()
+texts, bookmarks, snippets = parse_interviews()
 print() or pp.pprint(snippets)
 
 
+###############################################################################
+#    Heatmap of Correctness vs Confidence
+###############################################################################
 X = np.linspace(0, 1, 2)
 Y = np.linspace(1, 6, 6)
 
@@ -30,3 +33,30 @@ plt.close()
 hm = sns.heatmap(df)
 hm.invert_yaxis()
 
+
+###############################################################################
+#    Correctness of NYU students
+###############################################################################
+
+nyu_snippets = [x for x in snippets if x.subject in set([4168, 3316, 4281, 9112, 3787])]
+
+nyu_snippets
+
+###############################################################################
+#    CSV
+###############################################################################
+
+import csv
+with open('think_aloud_results.csv', "w") as csv_file:
+    writer = csv.writer(csv_file, delimiter=',')
+    writer.writerow(['subject', 'start', 'end', 'snippet', 'atom', 'confusingness', 'answer', 'confidence'])
+    for s in snippets:
+        writer.writerow([s.subject, s.start, s.end, s.snippet, s.atom, s.confusingness, s.answer, s.confidence])
+
+
+
+###############################################################################
+#    Grouping snippets by code
+###############################################################################
+
+pp.pprint(snippets[0].codes)
--- parse_snippets.py
+++ parse_snippets.py
@@ -4,8 +4,16 @@ import itertools
 import re
 #from frozendict import frozendict
 import glob
+from intervaltree import Interval, IntervalTree
 
 class Snippet:
+    SNIPPET_PAT = 'Snippet - (.*)'
+    ANSWER_PAT = 'Answer - (.*)'
+    CONFUSINGNESS_PAT = 'Confusingness - (.*)'
+    CONFIDENCE_PAT = 'Confidence - (.*)'
+    ATOM_PAT = '(.*[^-]) (Atom|Transformation)'
+    SECTION_PAT = 'Section - (.*)'
+
     def find_content(regex, overlap):
         element = next(x for x in overlap if re.match(regex, x['content']))
         match = re.match(regex, element['content'])
@@ -14,19 +22,21 @@ class Snippet:
     def create(subject, overlap):
         try:
             return Snippet(subject, overlap)
-        except:
+        except StopIteration: # this overlap doesn't descript a snippet (maybe it's a code inside a snippet)
             return None
 
     def __init__(self, subject, overlap):
-        snippet = Snippet.find_content('Snippet - (.*)', overlap)
+        snippet = Snippet.find_content(self.SNIPPET_PAT, overlap)
         self.subject = subject
         self.start = snippet['start']
         self.end = snippet['end']
         self.snippet = int(snippet['match'])
-        self.answer = Snippet.find_content('Answer - (.*)', overlap)['match']
-        self.confusingness = Snippet.find_content('Confusingness - (.*)', overlap)['match']
-        self.confidence = int(Snippet.find_content('Confidence - (.*)', overlap)['match'])
-        self.atom = Snippet.find_content('(.*[^-]) (Atom|Transformation)', overlap)['match']
+        self.answer = Snippet.find_content(self.ANSWER_PAT, overlap)['match']
+        self.confusingness = Snippet.find_content(self.CONFUSINGNESS_PAT, overlap)['match']
+        self.confidence = int(Snippet.find_content(self.CONFIDENCE_PAT, overlap)['match'])
+        self.atom = Snippet.find_content(self.ATOM_PAT, overlap)['match']
+        self.section = Snippet.find_content(self.SECTION_PAT, overlap)['match']
+        self.codes = []
 
     def __repr__(self):
        return '%s <%d, %d> (%d - %s %s) [%s - %d]' % (self.subject, self.start, self.end, self.snippet, self.atom, self.confusingness, self.answer, self.confidence)
@@ -44,6 +54,8 @@ def parse_interviews():
     interviews_glob = '/Users/dgopstein/nyu/confusion/think-aloud/qda_miner/*interview.RTF'
 
     all_snippets = []
+    all_bookmarks = []
+    all_text = {}
 
     inpath = '/Users/dgopstein/nyu/confusion/think-aloud/qda_miner/4281 interview.RTF'
     for inpath in glob.glob(interviews_glob):
@@ -56,14 +68,38 @@ def parse_interviews():
 
         bookmarks, stripped_rtf = extract_rtf.striprtf(doc)
 
+        for b in bookmarks:
+            b['subject'] = subject
+
+        all_bookmarks.extend(bookmarks)
+        all_text[subject] = stripped_rtf
+
         overlaps = extract_rtf.all_overlaps(bookmarks)
-        snippets = set(Snippet.create(subject, x) for x in overlaps if Snippet.create(subject, x) is not None)
+        snippet_list = [Snippet.create(subject, ol) for ol in overlaps if Snippet.create(subject, ol) if not None]
+        snippets = set(snippet_list)
+
+        add_codes_to_snippets(snippets, bookmarks, stripped_rtf)
 
         all_snippets.extend(sorted(snippets))
 
-    return all_snippets
+    return (all_text, all_bookmarks, all_snippets)
+
+# filter out the bookmarks that describe snippets and sections
+def is_code(b):
+    return not re.match('|'.join([Snippet.SNIPPET_PAT, Snippet.ANSWER_PAT, Snippet.CONFUSINGNESS_PAT, Snippet.CONFIDENCE_PAT, Snippet.ATOM_PAT, Snippet.SECTION_PAT]), b['content'])
+
+def add_codes_to_snippets(snippets, bookmarks, text):
+    snippet_intervals = [Interval(s.start, s.end, s) for s in snippets]
+    snippet_tree = IntervalTree(snippet_intervals)
+
+    for b in bookmarks:
+        if is_code(b):
+            snippet_set = snippet_tree[b['start']:b['end']]
+            if len(snippet_set) == 1: # there are many codes after the evaluation section
+                snippet_interval = next(iter(snippet_set))
+                b['text'] = text[b['start']:b['end']]
+                snippet_interval.data.codes.append(b)
 
 #pp.pprint(overlaps[1])
-#snippets = parse_interviews()
+#texts, bookmarks, snippets = parse_interviews()
 #print() or pp.pprint(parse_interviews())
-
