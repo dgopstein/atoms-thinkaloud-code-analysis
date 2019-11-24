de805eb5650b6d43e1550baa1c5dca0d35220e51
Dan Gopstein <dan@gopstein.com>
2019-11-24 17:55:39 -0500

    snippet correctness programmer experience and snippet type

--- code_analysis.py
+++ code_analysis.py
@@ -46,6 +46,108 @@ subject_groups = {4168: 'student',
  1867: 'librarian'}
 
 #%%############################################################################
+#    Atoms Types
+###############################################################################
+
+def transpose(a2d):
+    return [*zip(*a2d)]
+
+atom_type_array = [
+    ['implicit_predicate', 'C', 1],
+    ['operator_precedence', 'C', 11],
+    ['post_inc_dec', 'C', 13],
+    ['post_inc_dec', 'NC', 14],
+    ['pre_inc_dec', 'C', 19],
+    ['constant_variables', 'HC', 25],
+    ['constant_variables', 'HNC', 26],
+    ['macro_operator_precedence', 'C', 37],
+    ['pointer_arithmetic', 'HC', 45],
+    ['pointer_arithmetic', 'HNC', 46],
+    ['conditional_operator', 'C', 49],
+    ['conditional_operator', 'NC', 50],
+    ['arithmetic_as_logic', 'HC', 55],
+    ['arithmetic_as_logic', 'HNC', 56],
+    ['comma_operator', 'C', 61],
+    ['comma_operator', 'NC', 62],
+    ['preprocessor_in_statement', 'C', 71],
+    ['assignment_as_value', 'C', 73],
+    ['logic_as_control_flow', 'C', 79],
+    ['repurposed_variable', 'C', 85],
+    ['reversed_subscripts', 'C', 91],
+    ['unreachable', 'HC', 99],
+    ['unreachable', 'HNC', 100],
+    ['change_of_literal_encoding', 'C', 105],
+    ['omitted_curly_braces', 'C', 109],
+    ['type_conversion', 'C', 115]]
+
+atom_type_df = pd.DataFrame(atom_type_array, columns=['atom', 'confusingness', 'qid'])
+
+
+snippet_effectsize_array = [
+    [1,  0.32274861, 0.8958333, 1.0000000],
+    [3,  0.38765744, 0.7142857, 0.8979592],
+    [5,  0.00000000, 0.7346939, 0.7346939],
+    [7,  0.38592249, 0.8510638, 1.0000000],
+    [9,  0.37796447, 0.7551020, 0.8979592],
+    [11,  0.25819889, 0.6530612, 0.7959184],
+    [13,  0.54433105, 0.5833333, 0.9166667],
+    [15,  0.59230570, 0.5306122, 0.9183673],
+    [17,  0.49160514, 0.5102041, 0.8163265],
+    [19,  0.28022427, 0.6041667, 0.7500000],
+    [21,  0.16495722, 0.7551020, 0.8367347],
+    [23,  0.40526180, 0.5510204, 0.8163265],
+    [25,  0.14433757, 0.9791667, 1.0000000],
+    [27,           0, 1.0000000, 1.0000000],
+    [29,  0.08247861, 0.9795918, 0.9591837],
+    [37,  0.82915620, 0.2083333, 0.8958333],
+    [41,  0.11664237, 0.9183673, 0.9591837],
+    [43,  0.08333333, 0.4583333, 0.4166667],
+    [45,  0.35659298, 0.7346939, 0.9183673],
+    [47,  0.16386838, 0.6122449, 0.5102041],
+    [49,  0.47871355, 0.7708333, 1.0000000],
+    [51,  0.41666667, 0.7500000, 0.9583333],
+    [53,  0.49487166, 0.7551020, 1.0000000],
+    [55,  0.19364917, 0.8958333, 0.9583333],
+    [57,  0.08247861, 0.9591837, 0.9795918],
+    [59,  0.00000000, 0.9591837, 0.9591837],
+    [61,  0.66395281, 0.5000000, 0.9791667],
+    [63,  0.31943828, 0.4489796, 0.6530612],
+    [65,  0.00000000, 0.4897959, 0.4897959],
+    [67,  0.15593624, 0.2127660, 0.2978723],
+    [69,  0.71428571, 0.4897959, 1.0000000],
+    [71,  0.89214257, 0.2040816, 1.0000000],
+    [73,  0.59511904, 0.6458333, 1.0000000],
+    [75,  0.67813269, 0.3673469, 0.8979592],
+    [77,  0.52236453, 0.3265306, 0.7142857],
+    [79,  0.58925565, 0.2708333, 0.6875000],
+    [81,  0.90350790, 0.1224490, 0.9387755],
+    [83,  0.00000000, 0.5102041, 0.5102041],
+    [85,  0.28022427, 0.5208333, 0.6666667],
+    [87,  0.06734350, 0.4081633, 0.3673469],
+    [89,  0.45042330, 0.4897959, 0.7551020],
+    [91,  0.33333333, 0.5833333, 0.7500000],
+    [93,  0.47951222, 0.4081633, 0.6734694],
+    [95,  0.42605779, 0.5714286, 0.8367347],
+    [97,  0.08333333, 0.9375000, 0.9583333],
+    [99,  0.28571429, 0.9183673, 1.0000000],
+    [101,          0, 1.0000000, 1.0000000],
+    [103, 0.92421138, 0.1458333, 1.0000000],
+    [105, 0.79685973, 0.1428571, 0.8163265],
+    [107, 0.47951222, 0.4693878, 0.7346939],
+    [109, 0.16666667, 0.7083333, 0.7916667],
+    [111, 0.19166297, 0.5102041, 0.6326531],
+    [113, 0.38180177, 0.6326531, 0.8367347],
+    [115, 0.04811252, 0.8333333, 0.8125000],
+    [117, 0.31183152, 0.6734694, 0.8571429],
+    [119, 0.80952381, 0.2448980, 0.9387755]]
+
+
+snippet_effectsize_df = pd.DataFrame(snippet_effectsize_array, columns=['qid', 'effect-size', 'C-rate', 'NC-rate'])
+
+snippet_old_correct_rates_df = snippet_effectsize_df[['qid', 'C-rate']].rename(columns={'C-rate':'oldrate'}).append(
+        snippet_effectsize_df[['qid', 'NC-rate']].rename(columns={'NC-rate':'oldrate'}).add([1, 0]), sort=False)
+
+#%%############################################################################
 #    Parse survey data
 ###############################################################################
 
@@ -191,11 +293,14 @@ years_answers_df = subject_answer_sums_df.join(survey_first_lang_df[['Subject', 
 
 years_answers_df['years_experience'] = years_answers_df.apply(lambda row: 2019-row['Learned Year'], axis=1)
 
+years_answers_df['subject_group'] = years_answers_df['subject'].map(subject_groups)
+
 plt.close()
 sns.set(rc={'figure.figsize':(5,5)})
-years_vs_correctness_plot = sns.scatterplot(x='years_experience', y='answer', data=years_answers_df, s = 70)
+years_vs_correctness_plot = sns.scatterplot(x='years_experience', y='answer', hue='subject_group', data=years_answers_df, s = 70)
 years_vs_correctness_plot.set(ylim=(0,8.5),
                               xlabel="Years Programming", ylabel="Correct Answers")
+plt.legend(loc='lower right')
 plt.savefig('img/years_vs_correctness.pdf')
 
 #%%############################################################################
@@ -233,15 +338,36 @@ pprn(wrong_snippets)
 #    Which questions were most confusing
 ###############################################################################
 
-snippets_answer_sums = snippets_df.groupby(['snippet', 'answer'])
-snippets_answer_sums.agg({'size': 'sum'})
-snippet_group_rates = snippets_answer_sums.groupby(level=0).apply(lambda x: x / float(x.sum()))
-
 snippet_answer_groups = snippets_df.groupby(['snippet', 'answer'], as_index=False).size().to_frame('size').reset_index()
 snippet_answer_sums = snippet_answer_groups.groupby(['snippet', 'answer']).agg({'size': 'sum'})
-snippet_answer_rates = snippet_answer_sums.groupby(level=0).apply(lambda x: x / float(x.sum()))
+snippet_answer_rates = snippet_answer_sums.groupby(level=0).apply(lambda x: x / float(x.sum())).rename(columns={'size': 'rate'})
 
 snippet_answer_rates.reset_index(inplace=True)
 snippet_correct_rates = snippet_answer_rates[snippet_answer_rates['answer'] == 'Right']
+snippet_correct_rates = snippet_correct_rates.join(atom_type_df.set_index('qid'), on='snippet').join(snippet_old_correct_rates_df.set_index('qid'), on='snippet')
+snippet_correct_rates['oldrate_jitter'] = snippet_correct_rates['oldrate'] + (0.01 * snippet_correct_rates.index.map(lambda x: x % 10) * snippet_correct_rates['rate'].map(lambda x: 1 if x == 1 else 0))
+#snippet_correct_rates.sort_values(by='rate')
 
-snippet_correct_rates.sort_values(by='size')
+plt.close()
+snippet_correctness_plot = sns.scatterplot(x='oldrate_jitter', y='rate', hue='confusingness', data=snippet_correct_rates, s = 70, x_jitter=.2)
+snippet_correctness_plot.set(ylim=[0,1.03], xlim=[0,1.1], xlabel="Previous Correctness", ylabel="Current Correctness")
+
+for idx in range(0,len(snippet_correct_rates)):
+    x, y, text = snippet_correct_rates[['oldrate_jitter', 'rate', 'atom']].iloc[idx]
+    snippet_correctness_plot.text(x+0.008*len(text)**0.8, y-0.008*len(text)**0.8, text, size=5, color='black', weight='semibold', rotation=315, horizontalalignment='center', verticalalignment='center')
+
+plt.legend(loc='lower right')
+plt.savefig('img/snippet_correctness.pdf')
+
+#%%############################################################################
+#    Operator Precedence - Right for wrong reason
+###############################################################################
+
+right_wrong_reason = [{'snippet': s, 'code': c} for s in snippets for c in s.codes + s.discussion_codes if c['content'] == 'Correct for Wrong Reasons']
+
+op_prec_right_wrong_reason = [d for d in right_wrong_reason if d['snippet'].snippet == 11]
+
+pprn(op_prec_right_wrong_reason)
+
+
+
